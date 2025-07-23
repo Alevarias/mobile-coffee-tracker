7cef8e60da492a1e78c52f150c2bed2e41956b07
joey-wong-cuanschutz <joey.wong@cuanschutz.edu>
2025-07-23 14:19:56 -0600

    update Dao

--- app/src/main/java/com/example/coffeetracker/CoffeeDao.java
+++ app/src/main/java/com/example/coffeetracker/CoffeeDao.java
@@ -1,10 +1,21 @@
 package com.example.coffeetracker;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface CoffeeDao {
-    @Query("select * from CoffeeRecords order by Date")
+    @Query("select * from CoffeeRecords order by brewDate")
     public List<CoffeeRecords> getAll();
+
+    @Insert
+    public long insertCoffeeRecords (CoffeeRecords coffeeRecords);
+
+    @Delete
+    public void deleteCoffeeRecord(CoffeeRecords coffeeRecords);
+
 }
